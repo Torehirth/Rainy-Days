1e9927e1a315f9ecebe13b0753670d5a936d3ad2
Tore Mæland Hirth <torehirth@gmail.com>
2024-05-07 18:36:09 +0200

    corrected switch case to no fall through

--- js/ui/displayJacketInformation.mjs
+++ js/ui/displayJacketInformation.mjs
@@ -3,22 +3,25 @@ import { message } from "../components/message.mjs";
 // using a switch statement to render the HTML.
 export function displayJacketInformation(type, jacket, container) {
   switch (type) {
-    case "image":
+    case "image": {
       container.innerHTML = `
                               <img class="product-image-container" src="${jacket.images[0].src}" alt="${jacket.name}" ></img>
                             `;
       break;
-    case "title":
+    }
+    case "title": {
       container.innerHTML = `
                               <h1>${jacket.name}</h1>
                             `;
       break;
-    case "description":
+    }
+    case "description": {
       container.innerHTML = `
                               <p>${jacket.description}</p>
                              `;
       break;
-    case "sizes":
+    }
+    case "sizes": {
       for (const size of jacket.attributes[0].terms) {
         const sizeName = size.name;
         container.innerHTML += `
@@ -31,12 +34,14 @@ export function displayJacketInformation(type, jacket, container) {
                                  `;
       }
       break;
-    case "price":
+    }
+    case "price": {
       container.innerHTML = `
                               <p>$${jacket.prices.price}</p>
                              `;
       break;
-    case "details":
+    }
+    case "details": {
       let detailHTML = "";
       for (const detail of jacket.attributes[2].terms) {
         const jacketSpec = detail.name;
@@ -48,7 +53,9 @@ export function displayJacketInformation(type, jacket, container) {
       container.innerHTML = `<p class="product_details">${jacket.description}</p>` + `<br>` + detailHTML;
 
       break;
-    default:
+    }
+    default: {
       container.innerHTML = message("error", `Cant't show the ${type} at the moment..`);
+    }
   }
 }
